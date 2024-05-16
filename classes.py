c3246af256b79d84b4ee3a89d3511bf21c452238
Pavel <pashapasha-2013@mail.ru>
2024-05-16 19:57:19 +0300

    robot added

--- classes.py
+++ classes.py
@@ -11,17 +11,7 @@ class BasicSquareObject:
         self.color = color
         self.x = cords[0]
         self.y = cords[1]
-        self.rect = self.form_rect()
-
-    def get_size(self):
-        return (self.width, self.height)
-
-    def get_cords(self):
-        return (self.x, self.y)
-
-    def form_rect(self):
-        rect = pygame.Rect(self.x, self.y, self.width, self.height)
-        return rect
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
     def draw(self, screen: pygame.Surface):
         pygame.draw.rect(screen, self.color, self.rect)
@@ -29,26 +19,46 @@ class BasicSquareObject:
 
 class BasicTriangleObject:
     def __init__(
-        self, radius: int = 10, color: str = "Black", center_cords: tuple = (0, 0)
+        self, radius: int = 10, center_cords: tuple = (0, 0), dir: float = 90
     ) -> None:
-        self.color = color
+        self.color = 'Red'
         self.cx = center_cords[0]
         self.cy = center_cords[1]
         self.rad = radius
+        self.dir = -dir
 
     @staticmethod
-    def calculate_triangle_points(x: int, y: int, r:int) -> tuple:
-        x1 = x + r * math.cos(math.radians(30))
-        y1 = y + r * math.sin(math.radians(30))
-        x2 = x + r * math.cos(math.radians(150))
-        y2 = y + r * math.sin(math.radians(150))
-        x3 = x + r * math.cos(math.radians(270))
-        y3 = y + r * math.sin(math.radians(270))
+    def calculate_triangle_points(x: int, y: int, r:int, dir : int) -> tuple:
+        x1 = x + r * math.cos(math.radians(dir))
+        y1 = y + r * math.sin(math.radians(dir))
+        x2 = x + r * math.cos(math.radians(dir + 120))
+        y2 = y + r * math.sin(math.radians(dir + 120))
+        x3 = x + r * math.cos(math.radians(dir + 240))
+        y3 = y + r * math.sin(math.radians(dir + 240))
         return (x1, y1), (x2, y2), (x3, y3)
-
-    def move(self, delta_X : int, delta_y: int):
-        self.cx += delta_X
-        self.cy += delta_y 
     
     def draw(self, screen: pygame.Surface):
-        pygame.draw.polygon(screen, self.color, BasicTriangleObject.calculate_triangle_points(self.cx, self.cy, self.rad))
+        pygame.draw.polygon(screen, self.color, BasicTriangleObject.calculate_triangle_points(self.cx, self.cy, self.rad, self.dir))
+
+class Robot:
+    def __init__(self, cords : tuple = (10, 10), dir : float = 0, rad : int = 20) -> None:
+        self.color = 'Black'
+        self.hed_color = 'Red'
+        self.dir =  -1 * dir
+        self.x = cords[0]
+        self.y = cords[1]
+        self.rad = rad
+        self.head = BasicTriangleObject(self.rad / 2, self.calculate_triangle_head(self.x, self.y, self.rad, self.dir), -self.dir)
+
+    @staticmethod
+    def calculate_triangle_head(x, y, rad, dir):
+        xc = x + rad * math.cos(math.radians(dir))
+        yc = y + rad * math.sin(math.radians(dir))
+        return (xc, yc)
+
+
+    def draw(self, screen: pygame.Surface):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.rad)
+        self.head.draw(screen)
+        
+
--- main.py
+++ main.py
@@ -5,12 +5,14 @@ pygame.init()
 screen = pygame.display.set_mode((1500, 750))
 screen.fill((255, 255, 255))
 base = BasicSquareObject((50, 50), 'Black', (725, 355))
-target = BasicTriangleObject(30, 'red', (200, 200))
+target = BasicTriangleObject(30, (200, 200), 90)
+robot = Robot((400, 500), 0, 40)
 
 running = True
 while running:
 
     base.draw(screen)
+    robot.draw(screen)
     target.draw(screen)
     pygame.display.update()
     for event in pygame.event.get():
