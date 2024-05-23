3d19443b3323890e9430dd04e75db30465c7cc63
Pavel <pashapasha-2013@mail.ru>
2024-05-24 01:42:13 +0300

    basic preps

--- classes.py
+++ classes.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 import math
 
@@ -12,23 +13,32 @@ class BasicSquareObject:
         self.x = cords[0]
         self.y = cords[1]
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.visible = True
 
     def draw(self, screen: pygame.Surface):
-        pygame.draw.rect(screen, self.color, self.rect)
+        if self.visible:
+            pygame.draw.rect(screen, self.color, self.rect)
+        else:
+            pygame.draw.rect(screen, "White", self.rect)
+
+    def set_cords(self, cords):
+        self.x, self.y = cords
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
 
 class BasicTriangleObject:
     def __init__(
-        self, radius: int = 10, center_cords: tuple = (0, 0), dir: float = 90
+        self, radius: int = 10, center_cords: list = [0, 0], dir: float = 90
     ) -> None:
-        self.color = 'Red'
+        self.color = "Red"
         self.cx = center_cords[0]
         self.cy = center_cords[1]
         self.rad = radius
         self.dir = -dir
+        self.visible = True
 
     @staticmethod
-    def calculate_triangle_points(x: int, y: int, r:int, dir : int) -> tuple:
+    def calculate_triangle_points(x: int, y: int, r: int, dir: int) -> tuple:
         x1 = x + r * math.cos(math.radians(dir))
         y1 = y + r * math.sin(math.radians(dir))
         x2 = x + r * math.cos(math.radians(dir + 120))
@@ -36,23 +46,43 @@ class BasicTriangleObject:
         x3 = x + r * math.cos(math.radians(dir + 240))
         y3 = y + r * math.sin(math.radians(dir + 240))
         return (x1, y1), (x2, y2), (x3, y3)
-    
-    def move(self):
-        self.cx +=1
-        self.cy +=1
-    
+
+    def set_cords(self, cords):
+        self.cx = cords[0]
+        self.cy = cords[1]
+
     def draw(self, screen: pygame.Surface):
-        pygame.draw.polygon(screen, self.color, BasicTriangleObject.calculate_triangle_points(self.cx, self.cy, self.rad, self.dir))
+        if self.visible:
+            pygame.draw.polygon(
+                screen,
+                self.color,
+                BasicTriangleObject.calculate_triangle_points(
+                    self.cx, self.cy, self.rad, self.dir
+                ),
+            )
+
 
 class Robot:
-    def __init__(self, cords : tuple = (10, 10), dir : float = 0, rad : int = 20) -> None:
-        self.color = 'Black'
-        self.hed_color = 'Red'
-        self.dir =  -1 * dir
+    def __init__(self, cords: tuple = (10, 10), dir: float = 0, rad: int = 20) -> None:
+        self.color = "Black"
+        self.hed_color = "Red"
+        self.dir = -1 * dir
         self.x = cords[0]
         self.y = cords[1]
         self.rad = rad
-        self.head = BasicTriangleObject(self.rad / 2, self.calculate_triangle_head(self.x, self.y, self.rad, self.dir), -self.dir)
+        self.head = BasicTriangleObject(
+            self.rad / 2,
+            self.calculate_triangle_head(self.x, self.y, self.rad, self.dir),
+            -self.dir,
+        )
+
+    @staticmethod
+    def add_random_robot():
+        return Robot(
+            (random.randint(0, 1500), random.randint(0, 750)),
+            random.randint(0, 359),
+            40,
+        )
 
     @staticmethod
     def calculate_triangle_head(x, y, rad, dir):
@@ -60,10 +90,10 @@ class Robot:
         yc = y + rad * math.sin(math.radians(dir))
         return (xc, yc)
 
-
     def draw(self, screen: pygame.Surface):
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.rad)
         self.head.draw(screen)
+
 
 class Button:
     def __init__(self, x, y, width, height, text, color) -> None:
@@ -76,19 +106,29 @@ class Button:
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         self.is_hovered = False
 
-    def draw(self, screen:pygame.Surface):
+    def draw(self, screen: pygame.Surface):
         pygame.draw.rect(screen, self.color, self.rect)
-        font_surface = pygame.font.Font('fonts/Roboto-Black.ttf', 30).render(self.text, False, 'White')
-        screen.blit(font_surface, (self.x + self.width/4, self.y + self.height / 4))
-        
+        font_surface = pygame.font.Font("fonts/Roboto-Black.ttf", 30).render(
+            self.text, False, "White"
+        )
+        screen.blit(font_surface, (self.x + self.width / 4, self.y + self.height / 4))
+
     def check_hover(self, mouse_pos):
         self.is_hovered = self.rect.collidepoint(mouse_pos)
-        if self.is_hovered:
-            self.text = 'idi naxyi'
-        else:
-            self.text = 'OGO'
 
     def handle_event(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.is_hovered:
-            print('zalupa')
+        if (
+            event.type == pygame.MOUSEBUTTONDOWN
+            and event.button == 1
+            and self.is_hovered
+        ):
+            return True
+        else:
+            return False
 
+
+class MoveTargetButton(Button):
+    in_move = False
+
+    def __init__(self, x, y, width, height, text, color) -> None:
+        super().__init__(x, y, width, height, text, color)
--- main.py
+++ main.py
@@ -1,30 +1,70 @@
 import pygame
 from classes import *
-import keyboard
+
 pygame.init()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((1500, 750))
 screen.fill((255, 255, 255))
-base = BasicSquareObject((50, 50), 'Black', (725, 355))
-target = BasicTriangleObject(30, (200, 200), 90)
-robot = Robot((400, 500), 0, 40)
-keyboard.add_hotkey('W', lambda: target.move())
-button = Button(500, 50, 100, 50, 'ОГО', 'Black')
+base = BasicSquareObject((50, 50), "Black", (725, 355))
+target = BasicTriangleObject(30, [200, 200], 90)
+robots = []
+add_button = Button(1200, 0, 300, 50, "Добавить", "Green")
+remove_button = Button(1200, 50, 300, 50, "Удалить", "Red")
+move_target_button = MoveTargetButton(1200, 100, 300, 50, "Цель", "Black")
+move_base_button = MoveTargetButton(1200, 150, 300, 50, "База", "Yellow")
 
 running = True
 while running:
-    screen.fill('White')
+    screen.fill("White")
     base.draw(screen)
-    robot.draw(screen)
     target.draw(screen)
-    button.draw(screen)
+    add_button.draw(screen)
+    remove_button.draw(screen)
+    move_base_button.draw(screen)
+    move_target_button.draw(screen)
+    if len(robots) > 0:
+        for robot in robots:
+            robot.draw(screen)
     clock.tick(60)
-    button.check_hover(pygame.mouse.get_pos())
+    add_button.check_hover(pygame.mouse.get_pos())
+    remove_button.check_hover(pygame.mouse.get_pos())
+    move_target_button.check_hover(pygame.mouse.get_pos())
+    move_base_button.check_hover(pygame.mouse.get_pos())
     pygame.display.update()
     for event in pygame.event.get():
-        button.handle_event(event=event)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            cords = pygame.mouse.get_pos()
+        if (
+            event.type == pygame.MOUSEBUTTONDOWN
+            and MoveTargetButton.in_move
+            and not (cords[0] in range(1200, 1500) and cords[1] in range(0, 300))
+        ):
+            MoveTargetButton.in_move = False
+            if base.visible:
+                target.set_cords(cords=cords)
+                target.visible = True
+            else:
+                base.set_cords(pygame.mouse.get_pos())
+                base.visible = True
+        if add_button.handle_event(event=event) and not (MoveTargetButton.in_move):
+            robots.append(Robot.add_random_robot())
+        if (
+            remove_button.handle_event(event=event)
+            and len(robots) > 0
+            and not (MoveTargetButton.in_move)
+        ):
+            robots.pop(random.randint(0, len(robots) - 1))
+        if move_target_button.handle_event(event=event) and not (
+            MoveTargetButton.in_move
+        ):
+            MoveTargetButton.in_move = True
+            target.visible = False
+        if move_base_button.handle_event(event=event) and not (
+            MoveTargetButton.in_move
+        ):
+            MoveTargetButton.in_move = True
+            base.visible = False
 
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-    
